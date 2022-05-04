ad59a7bf639f31ad1c0d0bfcff8ccc50aab436f2
Alexander Popov <mehanik-pyx@mail.ru>
2022-05-04 15:37:41 +0700

    add cool table

--- src/components/ChartTable.vue
+++ src/components/ChartTable.vue
@@ -1,6 +1,38 @@
 <template>
   <div v-if="chartOptions.series">
     <chart :options="chartOptions" constructor-type="stockChart"/>
+
+    <!-- Насколько я понял, Highcharts не даёт возможности менять отображение даты в таблице,
+         поэтому добавил сюда таблицу из примера, по хорошему её надо вынести в отдельный компонент-->
+    <table v-if="tableData" id="datatable">
+      <caption>Cool table</caption>
+      <thead>
+      <tr>
+        <th>Date</th>
+        <th
+          :class="sortedBy === th && 'sort'"
+          v-for="th in tableData.columnNames"
+          :key="th"
+          @click="sortBy(th)"
+        >{{ capitalizeFirstLetter(th) }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="row in tableData.rowData"
+        :key="row.date"
+      >
+        <th>{{ parseDate(row.date) }}</th>
+        <td
+          v-for="dateItem in row.data"
+          :key="dateItem.name"
+        >
+          {{ dateItem.value }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -8,7 +40,7 @@
   import {Chart} from 'highcharts-vue'
   import Highcharts from 'highcharts'
   import stockInit from 'highcharts/modules/stock'
-  import response from '../assets/response.json'
+  // import response from '../assets/response.json'
   import exportData from 'highcharts/modules/export-data'
   import exportingInit from 'highcharts/modules/exporting'
 
@@ -31,12 +63,15 @@
           accessibility: {enabled: false},
           credits: {enabled: false}
         },
+        tableData: null,
+        sortedBy: null
       }
     },
     mounted() {
-      console.log(response.items);
-      this.chartOptions.series = this.dataMapper(response.items)
-      //TODO: this.request()
+      this.request()
+      // console.log(response.items);
+      // this.chartOptions.series = this.chartMapper(response.items)
+      // this.tableData = this.tableMapper(response.items)
     },
     methods: {
       request() {
@@ -44,7 +79,8 @@
           fetch('https://test-task-for-frontend.herokuapp.com/data')
             .then(res => res.json())
             .then(data => {
-              this.chartOptions.series = this.dataMapper(data.items)
+              this.chartOptions.series = this.chartMapper(data.items)
+              this.tableData = this.tableMapper(data.items)
               console.log(data.items);
             })
         } catch (e) {
@@ -57,23 +93,88 @@
       parseDate(timestamp) {
         return Highcharts.dateFormat('%A, %b %e, %Y', timestamp)
       },
-      dataMapper(items) {
+      chartMapper(items) {
+        console.log(items)
         items.forEach((item) => {
-          console.log(item)
           item.name = this.capitalizeFirstLetter(item.name)
-          console.log(item)
           item.data.map((i) => {
             i.x = i.date
             i.y = i.value
             return i
           });
         })
+        console.log(items)
         return items
+      },
+
+      tableMapper(items) {
+        let newDataObj = {}
+        newDataObj.columnNames = items.map(item => item.name)
+        newDataObj.rowData = []
+        items[0].data.forEach((row, idx) => {
+          let rowObj = {}
+          rowObj.date = row.date
+          rowObj.data = items.map(item => {
+            return {
+              value: item.data[idx].value,
+              name: item.name
+            }
+          })
+          newDataObj.rowData.push(rowObj)
+        })
+        return newDataObj
+      },
+      sortBy(field) {
+        let indexA
+        let indexB
+        this.tableData.rowData.sort((a, b) => {
+          indexA = a.data.findIndex(it => it.name === field);
+          indexB = b.data.findIndex(it => it.name === field);
+          return a.data[indexA].value - b.data[indexB].value
+        })
+        this.sortedBy = field
       }
     }
   }
 </script>
 
 <style scoped>
+#datatable {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #ebebeb;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+}
 
+#datatable caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+
+#datatable th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+
+#datatable td,
+#datatable th,
+#datatable caption {
+  padding: 0.5em;
+}
+
+#datatable thead tr,
+#datatable tr:nth-child(even) {
+  background: #f8f8f8;
+}
+
+#datatable tr:hover {
+  background: #f1f7ff;
+}
+
+.sort {
+  text-decoration: underline;
+}
 </style>

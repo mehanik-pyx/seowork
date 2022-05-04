2d001e830d8d22b45b045269ab4060b1b9b0e214
Alexander Popov <mehanik-pyx@mail.ru>
2022-05-04 14:45:29 +0700

    mock response, add table and mapping, change cart to stockChart. seems done

--- src/components/ChartTable.vue
+++ src/components/ChartTable.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <chart constructor-type="stockChart" :options="chartOptions"/>
-    <code>{{ chartOptions }}</code>
+  <div v-if="chartOptions.series">
+    <chart :options="chartOptions" constructor-type="stockChart"/>
   </div>
 </template>
 
 <script>
   import {Chart} from 'highcharts-vue'
-  import dataMapper from "@/utils/dataMapper";
   import Highcharts from 'highcharts'
   import stockInit from 'highcharts/modules/stock'
+  import response from '../assets/response.json'
+  import exportData from 'highcharts/modules/export-data'
+  import exportingInit from 'highcharts/modules/exporting'
 
+  exportingInit(Highcharts)
+  exportData(Highcharts)
   stockInit(Highcharts)
+
   export default {
     name: "ChartTable",
     components: {Chart},
@@ -19,19 +23,20 @@
       return {
         chartOptions: {
           title: {text: 'Some cool chart'},
-          series: [{
-            data: []
-          }],
-          tooltip: {
-            shared: true
+          series: [],
+          exporting: {
+            showTable: true,
+            tableCaption: 'Not so cool table'
           },
           accessibility: {enabled: false},
           credits: {enabled: false}
         },
       }
     },
-    created() {
-      this.request()
+    mounted() {
+      console.log(response.items);
+      this.chartOptions.series = this.dataMapper(response.items)
+      //TODO: this.request()
     },
     methods: {
       request() {
@@ -39,12 +44,31 @@
           fetch('https://test-task-for-frontend.herokuapp.com/data')
             .then(res => res.json())
             .then(data => {
-              this.chartOptions.series[0].data = dataMapper(data.items)
+              this.chartOptions.series = this.dataMapper(data.items)
               console.log(data.items);
             })
         } catch (e) {
           console.log(e)
         }
+      },
+      capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      },
+      parseDate(timestamp) {
+        return Highcharts.dateFormat('%A, %b %e, %Y', timestamp)
+      },
+      dataMapper(items) {
+        items.forEach((item) => {
+          console.log(item)
+          item.name = this.capitalizeFirstLetter(item.name)
+          console.log(item)
+          item.data.map((i) => {
+            i.x = i.date
+            i.y = i.value
+            return i
+          });
+        })
+        return items
       }
     }
   }
